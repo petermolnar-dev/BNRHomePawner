708cd3169b9fa516f2b9b56b28c10e4888da1248
petermolnar-hu <peter@petermolnar.hu>
2015-03-14 15:17:14 +0100

    Initial Commit

---
+++ Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by Peter Molnar on 14/03/2015.
+//  Copyright (c) 2015 Peter Molnar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homepwner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homepwner
+//
+//  Created by Peter Molnar on 14/03/2015.
+//  Copyright (c) 2015 Peter Molnar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
